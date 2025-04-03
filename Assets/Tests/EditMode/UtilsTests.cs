029f8e1284cbc5b67c9bab6c20e09a81fd5c59b5
Simon Pino-Buisson <98675268+spynob@users.noreply.github.com>
2025-04-02 23:21:06 -0400

    Updated Utils

--- Assets/Tests/EditMode/UtilsTests.cs
+++ Assets/Tests/EditMode/UtilsTests.cs
@@ -1,30 +1,27 @@
 using NUnit.Framework;
 using UnityEngine;
+using System;
 
 public class UtilsTests {
     //Cartesian to Spherical VElOCITY SPRING
+    private float tolerance = 0.0001f;
     [Test]
     public void CartesianToSphericalVelocitySpringCorrect() {
-        Vector3 expected = new Vector3(3.393f, 2.001f, 0.776457f);
-        Vector3 actual = Utils.CartesianToSphericalVelocity(new Vector3(3, 4, 5), new Vector3(Mathf.PI / 4, Mathf.PI / 6, 2), 0.05f);
-        float tolerance = 0.01f;
+        Vector3 expected = new Vector3(0.64333113848f, 0.40024040134f, 0.776457135308f);
+        Vector3 actual = Utils.CartesianToSphericalVelocity(new Vector3(3, 4, 5), new Vector3(Mathf.PI / 4, Mathf.PI / 6, 10), 0.05f);
         Debug.Log($"Expected: {expected}, Actual: {actual}, Distance: {Vector3.Distance(expected, actual)}");
         Assert.That(Vector3.Distance(expected, actual), Is.LessThan(tolerance), "Vector mismatch");
     }
 
     [Test]
     public void CartesianToSphericalVelocitySpringLength0() {
-        Vector3 expected = new Vector3(0, 0, 0);
-        Vector3 actual = Utils.CartesianToSphericalVelocity(new Vector3(3, 4, 5), new Vector3(Mathf.PI / 4, Mathf.PI / 6, 0), 0.05f);
-        float tolerance = 0.01f;
-        Debug.Log($"Expected: {expected}, Actual: {actual}, Distance: {Vector3.Distance(expected, actual)}");
-        Assert.That(Vector3.Distance(expected, actual), Is.LessThan(tolerance), "Vector mismatch");
+        Exception ex = Assert.Throws<ArgumentException>(() => Utils.CartesianToSphericalVelocity(new Vector3(3, 4, 5), new Vector3(Mathf.PI / 4, Mathf.PI / 6, 0), 0.05f));
+        Assert.That(ex.Message, Is.EqualTo("Length must be non-zero! This may be a cast from Vector2."));
     }
     [Test]
     public void CartesianToSphericalVelocitySpringTheta0() {
-        Vector3 expected = new Vector3(2.299f, 0, -4);
-        Vector3 actual = Utils.CartesianToSphericalVelocity(new Vector3(3, 4, 5), new Vector3(0, Mathf.PI / 6, 0), 0.05f);
-        float tolerance = 0.01f;
+        Vector3 expected = new Vector3(0.4f, 0, 5.09807621135f);
+        Vector3 actual = Utils.CartesianToSphericalVelocity(new Vector3(3, 4, 5), new Vector3(0, Mathf.PI / 6, 10), 0.05f);
         Debug.Log($"Expected: {expected}, Actual: {actual}, Distance: {Vector3.Distance(expected, actual)}");
         Assert.That(Vector3.Distance(expected, actual), Is.LessThan(tolerance), "Vector mismatch");
     }
@@ -32,26 +29,16 @@ public class UtilsTests {
     // Cartesian to spherical VELOCITY RIGID
     [Test]
     public void CartesianToSphericalVelocityRigidCorrect() {
-        Vector2 expected = new Vector3(3.393f, 2.001f, 2);
-        Vector2 actual = Utils.CartesianToSphericalVelocity(new Vector3(3, 4, 5), new Vector3(Mathf.PI / 4, Mathf.PI / 6, 2), 0.05f);
-        float tolerance = 0.01f;
+        Vector2 expected = new Vector2(0.64333113848f, 0.40024040134f);
+        Vector2 actual = Utils.CartesianToSphericalVelocity(new Vector3(3, 4, 5), new Vector2(Mathf.PI / 4, Mathf.PI / 6), 10, 0.05f);
         Debug.Log($"Expected: {expected}, Actual: {actual}, Distance: {Vector3.Distance(expected, actual)}");
         Assert.That(Vector3.Distance(expected, actual), Is.LessThan(tolerance), "Vector mismatch");
     }
 
     [Test]
-    public void CartesianToSphericalVelocityRigidLength0() {
-        Vector2 expected = new Vector3(0, 0, 0);
-        Vector2 actual = Utils.CartesianToSphericalVelocity(new Vector3(3, 4, 5), new Vector3(Mathf.PI / 4, Mathf.PI / 6, 0), 0.05f);
-        float tolerance = 0.01f;
-        Debug.Log($"Expected: {expected}, Actual: {actual}, Distance: {Vector3.Distance(expected, actual)}");
-        Assert.That(Vector3.Distance(expected, actual), Is.LessThan(tolerance), "Vector mismatch");
-    }
-    [Test]
     public void CartesianToSphericalVelocityRigidTheta0() {
-        Vector2 expected = new Vector3(2.299f, 0, 2);
-        Vector2 actual = Utils.CartesianToSphericalVelocity(new Vector3(3, 4, 5), new Vector3(0, Mathf.PI / 6, 2), 0.05f);
-        float tolerance = 0.01f;
+        Vector2 expected = new Vector2(0.4f, 0);
+        Vector2 actual = Utils.CartesianToSphericalVelocity(new Vector3(3, 4, 5), new Vector2(0, Mathf.PI / 6), 10, 0.05f);
         Debug.Log($"Expected: {expected}, Actual: {actual}, Distance: {Vector3.Distance(expected, actual)}");
         Assert.That(Vector3.Distance(expected, actual), Is.LessThan(tolerance), "Vector mismatch");
     }
@@ -59,53 +46,58 @@ public class UtilsTests {
     // Spherical to cartesian VELOCITY SPRING
     [Test]
     public void SphericalToCartesianVelocitySpringGeneral() {
-        Vector2 expected = new Vector3(3.2165f, 4.24995f, 5.1247f);
-        Vector2 actual = Utils.SphericalToCartesianVelocity(new Vector3(3.3934f, 2.001f, 0.776457f), new Vector3(Mathf.PI / 4, Mathf.PI / 6, 2));
-        float tolerance = 0.01f;
+        Vector3 expected = new Vector3(7.29089962562f, 17.6776695297f, 36.8692660986f);
+        Vector3 actual = Utils.SphericalToCartesianVelocity(new Vector3(3, 4, 5), new Vector3(Mathf.PI / 4, Mathf.PI / 6, 10));
         Debug.Log($"Expected: {expected}, Actual: {actual}, Distance: {Vector3.Distance(expected, actual)}");
         Assert.That(Vector3.Distance(expected, actual), Is.LessThan(tolerance), "Vector mismatch");
     }
 
     [Test]
     public void SphericalToCartesianVelocitySpringTheta0() {
-        Vector2 expected = new Vector3(2.8284f, 3, 2.8284f);
-        Vector2 actual = Utils.SphericalToCartesianVelocity(new Vector3(2, 9999, 3), new Vector3(0, Mathf.PI / 2, 2));
-        float tolerance = 0.01f;
+        Vector3 expected = new Vector3(25.9807621135f, -5, 15);
+        Vector3 actual = Utils.SphericalToCartesianVelocity(new Vector3(3, 4, 5), new Vector3(0, Mathf.PI / 6, 10));
         Debug.Log($"Expected: {expected}, Actual: {actual}, Distance: {Vector3.Distance(expected, actual)}");
         Assert.That(Vector3.Distance(expected, actual), Is.LessThan(tolerance), "Vector mismatch");
     }
 
     // Spherical to cartesian VELOCITY RIGID
     [Test]
-    public void SphericalToCartesianVelocityRigidGeneral() { }
-
-    [Test]
-    public void SphericalToCartesianVelocityRigidTheta0() { }
-
-    // Spherical to Cartesian COORDS
-    [Test]
-    public void SphericalToCartesianCoordsCorrect() {
-        Vector3 expected = new Vector3(3, -4, 5);
-        Vector3 actual = Utils.SphericalToCartesianCoords(new Vector3(0.9695f, 1.03f, 7.071f));
-        float tolerance = 0.01f;
+    public void SphericalToCartesianVelocityRigidGeneral() {
+        Vector3 expected = new Vector3(4.22903744714f, 21.2132034356f, 35.1014991456f);
+        Vector3 actual = Utils.SphericalToCartesianVelocity(new Vector2(3, 4), new Vector2(Mathf.PI / 4, Mathf.PI / 6), 10);
         Debug.Log($"Expected: {expected}, Actual: {actual}, Distance: {Vector3.Distance(expected, actual)}");
         Assert.That(Vector3.Distance(expected, actual), Is.LessThan(tolerance), "Vector mismatch");
     }
 
     [Test]
-    public void SphericalToCartesianCoordsWithZeros() {
-        Assert.AreEqual(new Vector3(0, 0, 0), Utils.SphericalToCartesianCoords(new Vector3(00.9695f, 1.03f, 0)));
+    public void SphericalToCartesianVelocityRigidTheta0() {
+        Vector3 expected = new Vector3(25.9807621135f, 0, 15);
+        Vector3 actual = Utils.SphericalToCartesianVelocity(new Vector2(3, 4), new Vector2(0, Mathf.PI / 6), 10);
+        Debug.Log($"Expected: {expected}, Actual: {actual}, Distance: {Vector3.Distance(expected, actual)}");
+        Assert.That(Vector3.Distance(expected, actual), Is.LessThan(tolerance), "Vector mismatch");
+    }
+
+    // Spherical to Cartesian COORDS
+    [Test]
+    public void SphericalToCartesianCoordsCorrect() {
+        Vector3 expected = new Vector3(6.12372435696f, -7.07106781187f, 3.53553390593f);
+        Vector3 actual = Utils.SphericalToCartesianCoords(new Vector3(Mathf.PI / 4, Mathf.PI / 6, 10));
+        Debug.Log($"Expected: {expected}, Actual: {actual}, Distance: {Vector3.Distance(expected, actual)}");
+        Assert.That(Vector3.Distance(expected, actual), Is.LessThan(tolerance), "Vector mismatch");
+    }
+
+    [Test]
+    public void SphericalToCartesianCoordsWithZerolength() {
+        Exception ex = Assert.Throws<ArgumentException>(() => Utils.SphericalToCartesianCoords(new Vector3(3, 4, 0)));
+        Assert.That(ex.Message, Is.EqualTo("Length must be non-zero! This may be a cast from Vector2."));
     }
 
     // Cartesian to spherical COORDS 
     [Test]
     public void CartesianToSphericalCoordsCorrect() {
-        Vector3 expected = new Vector3(0.9695f, 1.03f, 7.071f);
-        Vector3 actual = Utils.RelativeCartesianToSphericalCoords(new Vector3(3, -4, 5));
-        float tolerance = 0.01f;
+        Vector2 expected = new Vector2(6.12372435696f, -7.07106781187f);
+        Vector2 actual = Utils.SphericalToCartesianCoords(new Vector3(Mathf.PI / 4, Mathf.PI / 6), 10);
         Debug.Log($"Expected: {expected}, Actual: {actual}, Distance: {Vector3.Distance(expected, actual)}");
         Assert.That(Vector3.Distance(expected, actual), Is.LessThan(tolerance), "Vector mismatch");
     }
-
-    // Overall tests
 }
--- Assets/Tests/PlayMode/UtilsTests.cs
+++ Assets/Tests/PlayMode/UtilsTests.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 using UnityEngine.TestTools;
 
 public class UtilsTests {
-    [UnityTest]
-    public IEnumerator FreefallVelocityUpdateTest() {
-        GameObject manager = new GameObject();
+    float tolerance = 0.01f;
+    GameObject manager = new GameObject();
+
+    [OneTimeSetUp]
+    public void Setup() {
         manager.AddComponent<GameManager>();
+
+    }
+
+    [UnityTest]
+    public IEnumerator FreefallDisplacementTest() {
 
         Vector3 InitialVel = new Vector3(1, 2, 3);
         float timeStep = 0.1f;
         Vector3 expected = new Vector3(0.99f, 0.999f, 2.97f);
-        Vector3 actual = Utils.FreefallVelocityUpdate(InitialVel, timeStep);
-        float tolerance = 0.01f;
+        Vector3 actual = Utils.FreefallDisplacement(InitialVel, timeStep);
 
         Debug.Log($"Expected: {expected}, Actual: {actual}, Distance: {Vector3.Distance(expected, actual)}");
         Assert.That(Vector3.Distance(expected, actual), Is.LessThan(tolerance), "Vector mismatch");
@@ -21,23 +27,41 @@ public class UtilsTests {
         yield return null;
     }
 
+    // Relative Cartesian to spherical
     [UnityTest]
-    public IEnumerator CartesianToSphericalCoordsWithZeros() {
-        GameObject manager = new GameObject();
-        manager.AddComponent<GameManager>();
-        float tolerance = 0.001f;
-        Assert.That(Vector3.Distance(new Vector3(0, 0, GameManager.Instance.epsilonLength), Utils.RelativeCartesianToSphericalCoords(new Vector3(0, 0, 0))), Is.LessThan(tolerance), "Vector mismatch");
+    public IEnumerator RelativeCartesianToSphericalTest() {
+        Vector3 expected = new Vector3(2.172060543f, 1.030376827f, 7.0710678f);
+        Vector3 actual = Utils.RelativeCartesianToSphericalCoords(new Vector3(3, 4, 5));
+        Assert.That(Vector3.Distance(expected, actual), Is.LessThan(tolerance), "Vector mismatch");
         yield return null;
     }
 
     [UnityTest]
-    public IEnumerator SymmetryTestCoords() {
-        GameObject manager = new GameObject();
-        manager.AddComponent<GameManager>();
-        Vector3 SphericalCoords = new Vector3(3, 4, 5);
-        Vector3 CartesianCoords = Utils.SphericalToCartesianCoords(SphericalCoords);
-        float tolerance = 0.001f;
-        Assert.That(Vector3.Distance(SphericalCoords, Utils.RelativeCartesianToSphericalCoords(CartesianCoords)), Is.LessThan(tolerance), "Vector mismatch");
+    public IEnumerator CartesianToSphericalCoordsWithZeros() {
+        Vector3 expected = new Vector3(0, 0, GameManager.Instance.epsilonLength);
+        Vector3 actual = Utils.RelativeCartesianToSphericalCoords(new Vector3(0, 0, 0));
+        Assert.That(Vector3.Distance(expected, actual), Is.LessThan(tolerance), "Vector mismatch");
+        yield return null;
+    }
+
+    // Centripetal accel tests Rigid
+    [UnityTest]
+    public IEnumerator IsRadialMovementOutwardRigidTheta0Fast() {
+        Assert.IsTrue(Utils.IsRadialMovementOutwardRigid(new Vector3(0, Mathf.PI / 6), new Vector3(5, 5)));
+        yield return null;
+    }
+    [UnityTest]
+    public IEnumerator IsRadialMovementOutwardRigidTheta0Slow() {
+        Assert.IsTrue(Utils.IsRadialMovementOutwardRigid(new Vector3(0, Mathf.PI / 6), new Vector3(0, 0)));
+        yield return null;
+    }
+    [UnityTest]
+    public IEnumerator IsRadialMovementOutwardRigidThetaPITrue() {
+        Assert.IsTrue(Utils.IsRadialMovementOutwardRigid(new Vector3(Mathf.PI, Mathf.PI / 6), new Vector3(5, 5)));
+        yield return null;
+    }
+    public IEnumerator IsRadialMovementOutwardRigidThetaPIFalse() {
+        Assert.IsFalse(Utils.IsRadialMovementOutwardRigid(new Vector3(Mathf.PI, Mathf.PI / 6), new Vector3(0, 0)));
         yield return null;
     }
 }
